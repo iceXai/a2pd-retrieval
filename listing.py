f2ec1fb4a9299e81db031bc9441d000a479334ef
spaul <stephan.paul@posteo.net>
2023-08-17 15:04:53 +0200

    [listing] adapted the Listing class to the new processor structure/API's

--- listing.py
+++ listing.py
@@ -21,141 +21,29 @@ class Listing(ABC):
     Abstract base class that handles the suitable (a.k.a., inside a specified 
     AOI) file identification process and the compilation of a list with 
     corresponding file URL's
-    """        
-    @abstractmethod
-    def setup_listing_processor(self) -> None:
-        pass
-    
-    @abstractmethod
-    def compile_file_listing(self) -> pd.DataFrame:
-        """
-        compiles the listing of all suitable swath files per sensor that needs 
-        to be retrieved from the server and returns it
-        """
-        pass
-    
-    def set_cfg(self, cfg: object) -> None:
+    """
+    def __init__(self, cfg: object):
         """
         Parameters
         ----------
         cfg : object
             Configuration module loading/handling the config file
-
-        Returns
-        -------
-        None
-            Used to pass the configuration down to the listing module and all 
-            subsequent instances and modules
         """
-        self.cfg = cfg     
-        
-    def get_date_strings(self) -> list:
-        """
-        handles the splitting up of the datetime objects into single strings
-        independent of the used carrier/sensor
-        """
-        #get start/stop dates
-        START = self.cfg.get_start_date()
-        STOP = self.cfg.get_stop_date()
-        # generate year (yy) and day-of-year (jj) strings for covered range
-        dates = [START + timedelta(days = day_diff) \
-                 for day_diff in range(0, (STOP - START).days+1)]
-        
-        date_str = [(date.strftime('%Y'), date.strftime('%j'))
-                    for date in dates]
-        
-        return date_str
-
-# In[]    
-    
-class ModisListing(Listing):
-    """
-    Terra/Aqua MODIS listing child class tailored to the 
-    sensor-specific processing
-    """
-    
-    def setup_listing_processor(self) -> None:
-        #status
-        logger.info(f'Setup listing processor...')
+        self.cfg = cfg
         #set processor
-        self.proc = ModisListingProcessor()
-        self.proc.set_cfg(self.cfg)
-        self.proc.initialize_processor()
-
+        self._initialize_listing_processor()
     
-    def compile_file_listing(self) -> pd.DataFrame:
+    """ Internals """
+    @abstractmethod
+    def _initialize_listing_processor(self) -> None:
+        pass
+    
+    """ API for run """
+    def compile_file_listing(self) -> None:
         #status
         logger.info(f'Compile file listing...')        
         #retrieve date strings for specified processing period
-        date_str = self.get_date_strings()
-        
-        #loop over all dates
-        for yy, jj in date_str:
-            #status
-            logger.info(f'Retrieving file listing for {jj}/{yy}...')
-
-            #set current urls/listing file names
-            self.proc.set_current_url(yy, jj)
-            self.proc.set_current_lfn(yy, jj)
-
-            #check whether listing for specified date already exists
-            LISTING_EXISTS = self.proc.check_for_existing_listing()
-            if LISTING_EXISTS:
-                logger.info(f'File listing does already exist!')
-                self.proc.load_listing()
-                continue
-            
-            """ geoMeta/MXD03: identify swaths in AOI's """                
-            #get mxd03 listing file
-            DOWNLOAD_COMPLETED = self.proc.get_geometa_file()
-
-            #continue with next date in case something went wrong
-            if not DOWNLOAD_COMPLETED:
-                continue
-            
-            #process listing
-            self.proc.process_geometa_file()
-
-            """ Compile list of matched MXD02 swaths """
-            #get the mxd02 listign file
-            DOWNLOAD_COMPLETED = self.proc.get_mxd02_file()
-
-            #continue with next date in case something went wrong
-            if not DOWNLOAD_COMPLETED:
-                continue
-            
-            #further process listing; adding MXD02 file names by matching
-            self.proc.process_mxd02_listing_file()
-            
-            #output listing csv file
-            self.proc.save_listing()
-            
-        #returns the completed listing to the caller
-        return self.proc.get_listing()
-            
-
-# In[]
-
-class SlstrListing(Listing):
-    """
-    Sentinel3-A/B SLSTR listing child class tailored to the 
-    sensor-specific processing
-    """
-
-    def setup_listing_processor(self) -> None:
-        #status
-        logger.info(f'Setup listing processor...')
-        #set processor
-        self.proc = SlstrListingProcessor()
-        self.proc.set_cfg(self.cfg)
-        self.proc.initialize_processor()
-    
-    
-    def compile_file_listing(self):
-        #status
-        logger.info(f'Compile file listing...')        
-        #retrieve date strings for specified processing period
-        date_str = self.get_date_strings()
+        date_str = self.proc.get_date_strings()
         
         #loop over all dates
         for yy, jj in date_str:
@@ -188,7 +76,32 @@ class SlstrListing(Listing):
             
         #returns the completed listing to the caller
         return self.proc.get_listing()
- 
+
+
+# In[]    
+    
+class ModisListing(Listing):
+    """
+    Terra/Aqua MODIS listing child class tailored to the 
+    sensor-specific processing
+    """
+    def _initialize_listing_processor(self) -> None:
+        #status
+        logger.info(f'Setup retrieval processor...')
+        self.proc = ModisListingProcessor(self.cfg) 
+            
+
+# In[]
+
+class SlstrListing(Listing):
+    """
+    Sentinel3-A/B SLSTR listing child class tailored to the 
+    sensor-specific processing
+    """
+    def _initialize_listing_processor(self) -> None:
+        #status
+        logger.info(f'Setup retrieval processor...')
+        self.proc = SlstrListingProcessor(self.cfg) 
     
 
 # In[]
@@ -198,14 +111,9 @@ class OlciListing(Listing):
     Sentinel3-A/B OLCI listing child class tailored to the 
     sensor-specific processing
     """
-    
-    def setup_listing_processor(self) -> None:
+    def _initialize_listing_processor(self) -> None:
         pass
-       
-    def compile_file_listing(self):
-        pass
-
-    
+ 
 
 # In[]
 
@@ -214,13 +122,9 @@ class ViirsListing(Listing):
     Suomi-NPP/NOOA20 VIIRS listing child class tailored to the 
     sensor-specific processing
     """
-            
-    def setup_listing_processor(self) -> None:
+    def _initialize_listing_processor(self) -> None:
         pass
-    
-    
-    def compile_file_listing(self):
-        pass
+
 
          
  
