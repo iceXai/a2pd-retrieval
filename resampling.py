826c8c1c97b0438ed2ac6c5560fddd10e58dec36
spaul <stephan.paul@posteo.net>
2023-10-18 16:00:18 +0200

    [resampling] also moved the respective resampling dataclasses to the resampling module

--- resampling.py
+++ resampling.py
@@ -6,129 +6,101 @@
 # In[] 
 from pyresample.kd_tree import get_neighbour_info
 from pyresample.kd_tree import get_sample_from_neighbour_info
+from dataclasses import dataclass
+from typing import List, Dict
+
 import pyresample as pr
 import numpy as np
+
+from aoi import AoiGrid
+from data import SwathVariable
+from data import DataVariable
 
 
 # In[]
 
-class ResampleHandler(object):
+@dataclass
+class ResampledVariable(SwathVariable):
+    """ Databaseclass to keep track of a resampled variable """
+    aoi: str
+    data: np.array
+    
+    @property
+    def shape(self) -> tuple:
+        return self.data.shape
+
+@dataclass
+class ResampleStack:
+    """ 
+    Container class to take on a DataStack group and resample it; turning 
+    all DataVariable's into ResampledVariable's
     """
-    Handles all the resample process of the current tobe processed swath data
-    """
-    def __init__(self, host_class: object):
-        #keep instance of the host class to use this as nestes class
-        self.ref = host_class
-        #for the grouping/stacking process
-        self.coord = {}
-        self.stack = {}
-        self.names = {}
-        #for the overall resampled data
-        self.resampled_aoi_data = {}
+    variables: List[DataVariable]
+    lon: DataVariable
+    lat: DataVariable
+    aoi: AoiGrid
     
-    """ Getters/Setters """
-    def get_resampled_data(self) -> dict:
-        return self.resampled_aoi_data
+    def __len__(self) -> int:
+        return len(self.variables)
     
-        
-    """ Resampling """
-    def add_data_to_group(self, var: str, lon: str, lat: str) -> None:
-        """
-        Parameters
-        ----------
-        var : str
-            Name of the varibale (keys from the variables to process) to be 
-            added
-        lon : str
-            Name of the longitude variable (taken from the resample
-                                            dictionary)
-        lat : str
-            Name of the latitude variable (also taken from the resample 
-                                           dictionary)
-
-        Returns
-        -------
-        None
-            Adds the data (names and coordinates) to the global dictionaries 
-            to be grouped afterwards
-        """
-        variable  = self.ref.data.get_data(var)
-        longitude = self.ref.data.get_data(lon)
-        latitude  = self.ref.data.get_data(lat)
-
-        #check if respective group already exists
-        key = f'{lon}{lat}'
-        if key not in self.coord.keys():
-            #if not create it 
-            self.coord[key] = [longitude,latitude]
-            self.stack[key] = []
-            self.names[key] = []
-        #place dataset names and actual data in respective dicts
-        self.stack[key].append(variable)
-        self.names[key].append(var)
+    def __iter__(self):
+        return iter(self.variables)
     
-        
-    def add_groups_to_resample_stack(self) -> None:
-        """
-        Stacks the already built groups for the final resample process
-        """
-        stack = {}
-        for idx,key in enumerate(self.coord):
-            stack[f'grp{idx}'] = {'grid': self.coord[key],
-                                  'dset': self.names[key],
-                                  'data': np.stack(self.stack[key],axis=2)
-                                  }
-        #return and clean-up
-        self.resample_stack = stack
-        self._reset_grouping_data()
-
-        
-    def _reset_grouping_data(self) -> None:
-        self.coord = {}
-        self.stack = {}
-        self.names = {}
-        
-        
-    def resample(self, aoi: str) -> dict:
-        #load resampled data into a data dictionary
-        rsd = {}
+    def __getitem__(self, item: str) -> SwathVariable:
+        if item in self.names:
+            return [var for var in self.variables if item == var.name][0]
+        else:
+            return None
     
-        #retrieve aoi grid to resample to
-        aoi_grid = self.ref.aoi.get_aoi(aoi).get_grid()
-
-        #return reference-grid latitude/longitude
-        ref_grid_lon, ref_grid_lat = aoi_grid.get_lonlats()
-        rsd['lon'] = ref_grid_lon
-        rsd['lat'] = ref_grid_lat
-        #retrieve resample group keys
-        grp_keys = self.resample_stack.keys()
+    @property
+    def names(self) -> List[str]:
+        return [var.name for var in self.variables]
     
-        #loop through groups
-        for grp in grp_keys:
-            #get lon/lat values
-            lon = self.resample_stack[grp]['grid'][0]
-            lat = self.resample_stack[grp]['grid'][1]
-            
-            #set-up swath definition
-            swath_def = pr.geometry.SwathDefinition(lons=lon, lats=lat)
-            #resample using kd tree
-            data_stack = self.resample_stack[grp]['data']
-            rs = self._kd_tree_resample(swath_def, aoi_grid, data_stack)
-
-            #append to dict
-            names_stack = self.resample_stack[grp]['dset']
-            rsd = {**rsd,**dict(zip(names_stack, rs))}
-            
-        self.resampled_aoi_data[aoi] = rsd
+    @property
+    def datatypes(self) -> List[str]:
+        return [var.datatype for var in self.variables]
+    
+    @property
+    def size(self) -> int:
+        return self.__len__()
+    
+    @property
+    def datastack(self) -> np.array:
+        stack = [var.data for var in self.variables]
+        return np.stack(stack,axis=2)
+    
+    @property
+    def metadata(self) -> List[Dict]:
+        return [var.meta for var in self.variables]
+    
+    def resample(self):
+        #stack available data
+        STACK = self.datastack
+        #set-up swath definition
+        LON = self.lon.data
+        LAT = self.lat.data
+        SWATH_DEF = pr.geometry.SwathDefinition(lons = LON, lats = LAT)
+        #resample using kd tree
+        AOI_GRID = self.aoi
+        self.stack = self._kd_tree_resample(SWATH_DEF, AOI_GRID, STACK)
+        #self.stack[np.where(self.stack == 0.0)] = np.nan
         
-
+    def export(self) -> List[ResampledVariable]:
+        NAMES = self.names
+        AOI_ID = self.aoi.area_id
+        DATATYPE = 'resampled'
+        STACK = self.stack
+        META = self.metadata
+        return [ResampledVariable(name, DATATYPE, META[idx], AOI_ID, 
+                                  STACK[idx]) 
+                for idx, name in enumerate(NAMES)]
+    
     def _kd_tree_neighbours(self, swath_def, aoi_grid) -> tuple:
         in_idx, out_idx, idx, _ = get_neighbour_info(swath_def,
                                                      aoi_grid,
                                                      radius_of_influence=5000,
                                                      neighbours=1)
         return in_idx, out_idx, idx
-    
     
     def _kd_tree_resample(self, swath_def, aoi_grid, stack) -> np.array:
         in_idx, out_idx, idx = self._kd_tree_neighbours(swath_def, aoi_grid)
