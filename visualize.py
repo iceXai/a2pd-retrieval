dcf139840d91ef4b63b0a08527a930780ceb9003
spaul <stephan.paul@posteo.net>
2023-10-23 16:18:59 +0200

    [viz] added further investigation

--- visualize.py
+++ visualize.py
@@ -11,12 +11,13 @@ import h5py
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 # In[]
 
 path = 'C:/data/testretrieval'
 testfile1 = 'ter_modis_2020245_005000_prod-nt-v1p0_brunt.h5'
-testfile2 = 's3b_slstr_2020245_075847_prod-nt-v1p0_brunt.h5'
+testfile2 = 's3a_slstr_2020247_010751_prod-nt-v1p0_brunt.h5'
 test1 = h5py.File(os.path.join(path,testfile1), "r")
 test2 = h5py.File(os.path.join(path,testfile2), "r")
 
@@ -26,8 +27,11 @@ print(test2['bt'].keys())
 fig, axs = plt.subplots(figsize=(40, 60),nrows=1, ncols=3)
 axs = axs.flatten()
 axs[0].imshow(test1['mod02']['ch20'][:])
-axs[1].imshow(test2['bt']['s8n'][:])
-axs[2].imshow(test2['bt']['s9exo'][:])
+ttt = test2['bt']['s8_nadir'][:]
+ttt[np.where(ttt==0.0)] = np.nan
+ttt[np.where(ttt==-32768.)] = np.nan
+axs[1].imshow(ttt)
+axs[2].imshow(test2['bt']['s8_oblique'][:])
 #plt.tight_layout()
 #plt.imshow(test['mod02']['ch20'][:])
 
